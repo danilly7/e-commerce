c51b828dd3cb5d83d969d206fcfc7bec5dac3c4e
Danilly Condori <danilly7@gmail.com>
2024-10-08 20:36:58 +0200

    Exercise 1

--- js/shop.js
+++ js/shop.js
@@ -1,68 +1,4 @@
 // If you have time, you can move this variable "products" to a json or js file and load the data in this js. It will look more professional
-var products = [
-    {
-        id: 1,
-        name: 'cooking oil',
-        price: 10.5,
-        type: 'grocery',
-        offer: {
-            number: 3,
-            percent: 20
-        }
-    },
-    {
-        id: 2,
-        name: 'Pasta',
-        price: 6.25,
-        type: 'grocery'
-    },
-    {
-        id: 3,
-        name: 'Instant cupcake mixture',
-        price: 5,
-        type: 'grocery',
-        offer: {
-            number: 10,
-            percent: 30
-        }
-    },
-    {
-        id: 4,
-        name: 'All-in-one',
-        price: 260,
-        type: 'beauty'
-    },
-    {
-        id: 5,
-        name: 'Zero Make-up Kit',
-        price: 20.5,
-        type: 'beauty'
-    },
-    {
-        id: 6,
-        name: 'Lip Tints',
-        price: 12.75,
-        type: 'beauty'
-    },
-    {
-        id: 7,
-        name: 'Lawn Dress',
-        price: 15,
-        type: 'clothes'
-    },
-    {
-        id: 8,
-        name: 'Lawn-Chiffon Combo',
-        price: 19.99,
-        type: 'clothes'
-    },
-    {
-        id: 9,
-        name: 'Toddler Frock',
-        price: 9.99,
-        type: 'clothes'
-    }
-]
 
 // => Reminder, it's extremely important that you debug your code. 
 // ** It will save you a lot of time and frustration!
@@ -75,10 +11,39 @@ var cart = [];
 var total = 0;
 
 // Exercise 1
+// 1. Loop for to the array products to get the item to add to cart
+// 2. Add found product to the cart array
+
+console.log("array products:", products);
+
+const buscarProducts = (valor) => products.findIndex(products => products.id === valor);
+//console.log("prueba en array products, buscando id 3 muestra posición:", buscarProducts(3));
+
+const buscarCart = (valor) => cart.findIndex(cart => cart.id === valor);
+
 function buy(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array
+    let positionProducts = buscarProducts(id);
+    let positionCart = buscarCart(id);
+    let newItem = 0;
+
+    if (positionProducts < 0) {
+        alert("Error: this product is not in our list of products.")
+    } else {
+        if (positionCart < 0) {
+            newItem = products[positionProducts];
+            newItem.quantity = 1;
+            console.log("new item que se añadirá al cart:", newItem);
+
+            cart.push(newItem);
+        } else {
+            cart[positionCart].quantity += 1;
+            console.log("item id que sumará quantity:", cart[positionCart].id);
+        }
+    }
 }
+console.log("Updated cart:", cart);
+console.table(cart);
+//console.log("prueba en array cart, buscando id 7 que no está añadido:", buscarCart(7));
 
 // Exercise 2
 function cleanCart() {
